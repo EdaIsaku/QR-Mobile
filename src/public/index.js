8b10f86b7692499e5535a213c0169301a4b4aeb2
Eda <eda.isaku@yahoo.com>
2022-09-22 23:33:07 +0200

    FIXED: Canvas reset on clearInput/Reset button

--- src/public/index.js
+++ src/public/index.js
@@ -1,15 +1,41 @@
 const inputNr = document.getElementsByClassName("inputNr")[0];
 const submitBtn = document.getElementsByClassName("submitBtn")[0];
+const clearBtn = document.getElementsByClassName("clearBtn")[0];
 const qrCode = document.getElementById("qrcode");
 
 let options = {
     text: `${inputNr.value}`,
 };
 
-submitBtn.addEventListener("click", () => {
-    // qrCode.clearRect(0, 0, canvas.width, canvas.height);
+let generatedQRCode;
 
-    new QRCode(qrCode, options);
-    // inputNr.value = "";
-    console.log("clicked");
+//Generate QRCODE and show image
+const resetCode = () => {
+    generatedQRCode.clear();
+};
+
+const generateCode = () => {
+    if (generatedQRCode) {
+        resetCode();
+        generatedQRCode = new QRCode(qrCode, options);
+        return;
+    }
+    generatedQRCode = new QRCode(qrCode, options);
+};
+
+submitBtn.addEventListener("click", () => {
+    if (inputNr.value == "") {
+        return;
+    }
+    generateCode();
+});
+
+inputNr.addEventListener("keyup", () => {
+    if (inputNr.value === "") {
+        resetCode();
+    }
+});
+
+clearBtn.addEventListener("click", () => {
+    resetCode();
 });
